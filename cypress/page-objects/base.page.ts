b492167a4dc057ba3e4cacfc3fb2182911166397
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2023-10-11 19:26:44 +0200

    modified login tests

--- cypress/page-objects/base.page.ts
+++ cypress/page-objects/base.page.ts
@@ -5,9 +5,12 @@ simpleChineseLink = '[lang="zh-cn"]'
 englishLink = '[lang="en"]'
 loginBtn = '.chr-header__user-zone .chr-button__text'
 signInHomePageBtn = 'button.chr-button.chr-button--light.chr-button--primary.chr-button--sm .chr-button__text '
-myAccountBtn = 'a.chr-button.chr-button--link.chr-button--lg.chr-button--light.chr-button--icon-left .chr-button__text.chr-action'
+myAccountBtn = '.chr-button.chr-button--link.chr-button--lg.chr-button--light.chr-button--icon-left'
+//myAccountBtn = 'a.chr-button.chr-button--link.chr-button--lg.chr-button--light.chr-button--icon-left .chr-button__text.chr-action'
 userInput = '#username'
 passwdInput = '#password'
+ErrorMessage =
+'.align-items-center.d-inline-flex.mb-5 > .chr-color-red-alert.chr-label.content-zone'
 signInModalWindowBtn = 'button.chr-button.chr-button--primary.chr-button--lg.chr-button--light.chr-button--full-width'
 // Page Object Methods
 
@@ -26,28 +29,22 @@ setCookie() {
     })
   }
 
-  login(
-    loginUsername,
-    loginPassword,
-  ) {
+  login(loginUsername, loginPassword) {
     cy.get(this.signInHomePageBtn).click()
     cy.get(this.userInput).click().type(loginUsername, {force: false, delay: 0})
     cy.get(this.passwdInput).click().type(loginPassword, {force: false, delay: 0})
     cy.get(this.signInModalWindowBtn).click()
   }
 
-  invalidlogin(
-    invalidUsername = 'invalid username',
-    invalidPassword = 'invalid password',
-  ) {
-    cy.get(this.signInHomePageBtn).click()
-    cy.get(this.userInput).click().type(invalidUsername)
-    cy.get(this.passwdInput).type(invalidPassword)
-    cy.get(this.signInModalWindowBtn).click()
-  }
 
   clickMyAccount() {
     cy.get(this.myAccountBtn).click()
   }
 
+  displayErrorMessage() {
+    cy.get(this.ErrorMessage).should(
+      'contain.text',
+      'The email address and password that you entered did not match our records.',
+    )
+  }
 }
