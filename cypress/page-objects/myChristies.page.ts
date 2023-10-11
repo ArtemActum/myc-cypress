6b254903111bbd02888683cb04ee394ec3a4eae8
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2023-10-11 19:28:11 +0200

    MyC page added

--- cypress/page-objects/myChristies.page.ts
+++ cypress/page-objects/myChristies.page.ts
@@ -2,5 +2,16 @@ import Base from './base.page'
 
 export default class MyChristies extends Base {
 
+  public KYCheaderComponent() {
+    return cy.get('div.myc-account-progress')
+  }
+
+  public myAccountProgressBarText() {
+    return cy.get('span.mr-2')
+  }
+
+  public myAccountProgressBarTextIncludes(expectedNumber: number) {
+    this.myAccountProgressBarText().should('include.text', expectedNumber)
+  }
 
   }
