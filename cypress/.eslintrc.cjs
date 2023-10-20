262e3320083a8ae17cfd07e22ceb7319e3405ba1
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2023-10-20 14:19:09 +0200

    modified eslint config

--- cypress/.eslintrc.cjs
+++ cypress/.eslintrc.cjs
@@ -1,10 +1,6 @@
 /* eslint-env node */
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	root: true,
