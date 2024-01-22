c8bbe7bd0558011fd2d5f0ac70315b626b913674
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2024-01-22 17:07:04 +0100

    schemas for api tests added

--- cypress/support/constants/apis.ts
+++ cypress/support/constants/apis.ts
@@ -38,3 +38,66 @@ export const schemaGetBooking = {
 		},
 	},
 }
+
+export const schemaCreateBooking = {
+	type: 'object',
+	required: ['bookingid', 'booking'],
+	properties: {
+		bookingid: {
+			type: 'number',
+		},
+		booking: {
+			type: 'object',
+			required: [
+				'firstname',
+				'lastname',
+				'totalprice',
+				'depositpaid',
+				'bookingdates',
+				'additionalneeds',
+			],
+			properties: {
+				firstname: {
+					type: 'string',
+				},
+				lastname: {
+					type: 'string',
+				},
+				totalprice: {
+					type: 'number',
+				},
+				depositpaid: {
+					type: 'boolean',
+				},
+				bookingdates: {
+					type: 'object',
+					required: ['checkin', 'checkout'],
+					properties: {
+						checkin: {
+							type: 'string',
+						},
+						checkout: {
+							type: 'string',
+						},
+					},
+				},
+				additionalneeds: {
+					type: 'string',
+				},
+			},
+		},
+	},
+}
+
+export const schemaGetBookingIDs = {
+	type: 'array',
+	items: {
+		type: 'object',
+		required: ['bookingid'],
+		properties: {
+			bookingid: {
+				type: 'number',
+			},
+		},
+	},
+}
