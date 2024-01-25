290512094806191d38544fdc935d9f659e6e9cf8
WMC\artem.mindsadyrov <artem.mindsadyrov@actumdigital.com>
2024-01-25 12:54:44 +0100

    requests added for API tests

---
+++ cypress/API/booking.request.ts
@@ -0,0 +1,32 @@
+export const NewBooking = {
+	firstname: 'John',
+	lastname: 'Doe',
+	totalprice: 200,
+	depositpaid: true,
+	bookingdates: {
+		checkin: '2023-11-01',
+		checkout: '2023-11-05',
+	},
+	additionalneeds: 'Breakfast',
+}
+
+export const UpdatedBookingData = {
+	firstname: 'Artem',
+	lastname: 'QA',
+	totalprice: 300,
+	depositpaid: true,
+	bookingdates: {
+		checkin: '2023-12-01',
+		checkout: '2023-12-05',
+	},
+	additionalneeds: 'lunch',
+}
+
+export const PartialUpdatedBookingData = {
+	totalprice: 3000,
+}
+
+export const Auth = {
+	username: 'admin',
+	password: 'password123',
+}
